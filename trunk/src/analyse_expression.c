757fc029672989b9723e45e9f9d7a582a08e0b68
momssesystem <momssesystem@9c1addf4-ab37-11dd-b08a-b13dd1f1ca2d>
2008-11-20 16:27:37 +0000

    voir modification.txt

--- trunk/src/analyse_expression.c
+++ trunk/src/analyse_expression.c
@@ -14,64 +14,130 @@
 
 
 /*----------- Tableau des fonctions internes(commande.h) ---------- */
-char * nom_fonction[NB_FONCTION] = { "pwd", "cd", "history", "builtins", "kill", "times", "exit" };
-fonction  tableau_fonction[NB_FONCTION] = { pwd, cd , history, builtins, killer, times, toexit};
-
-/*Le nom de la fonction n'est pas obligé de corespondre a la fonction (ex: "kill" associé a killer()), ca fonctionne.*/
+char * nom_fonction[NB_FONCTION] = { "pwd", "cd", "history", "builtins", 
+				     "kill", "times", "exit" };
+fonction  tableau_fonction[NB_FONCTION] = { pwd, cd , history, builtins, 
+					    killer, times, toexit};
 /*-----------------------------------------------------------------*/
 
 int ecrire_history(char ** arguments){
-  int argc = LongueurListe(arguments);
+  int argc = 10; //LongueurListe(arguments);
   int fichier = open("history.tmp", O_WRONLY | O_CREAT | O_APPEND, 0644);
   if(fichier == -1){
     perror("");
     return 1;
   }
-
   int i;
   char * espace = " ";
   for(i = 0; i < argc; i++){
     write(fichier, arguments[i], strlen(arguments[i]) * sizeof(char));
     write(fichier, espace, sizeof(char));
-  }
-  
+  }  
   char * back = "\n";
   write(fichier, back, sizeof(char));
   close(fichier);  
   return 0;
 }
 
-/*Execute le tube suivant : gauche | droite */
-static void tube(Expression * gauche, Expression * droite){
 
- if(fork()==0){ //Processus fils du mini Shell
+static void tube(Expression * gauche, Expression * droite){
     int tube[2];
     pipe(tube);
-    //int status;
-
-    pid_t processus = fork();
-    switch(processus){
-    case 0: //fils
-      dup2(tube[1], 1);
+    switch(fork()){
+    case 0://fils
       close(tube[0]);
+      dup2(tube[1], 1);
       close(tube[1]);
-      execvp(gauche->arguments[0], gauche->arguments);
-      perror("exec");
+      if(gauche->type == SIMPLE){
+	execvp(gauche->arguments[0], gauche->arguments);
+	perror("exec");
+      }
+      else
+	analyse_cmd(gauche);
       break;
-    default: //pere
-      //waitpid(0, &status, WNOHANG);
+    default://pere
       wait(NULL);
+      close(tube[1]);
       dup2(tube[0],0);
       close(tube[0]);
-      close(tube[1]);
-      execvp(droite->arguments[0], droite->arguments);
-      perror("exec");
+      if(droite->type == SIMPLE){
+	execvp(droite->arguments[0], droite->arguments);
+	perror("exec");
+      }
+      else
+	analyse_cmd(droite);
+      break;
+    } 
+}
+
+
+
+void executer_cmd(Expression * e){
+  bool trouver = false;
+  char * nom_commande = e->arguments[0];
+  for(int i=0; i < NB_FONCTION; i++){
+    if( strcmp(nom_fonction[i], nom_commande) == 0 ){
+      trouver = true;
+      fonction f = tableau_fonction[i];
+      int retour = (f)(e->arguments); 
+      //afficher_prompt(retour);
       break;
     }
   }
+  if(! trouver){
+    int retour;
+    if(fork() == 0){
+      retour = execvp(e->arguments[0], e->arguments );
+      if(retour == -1)
+	fprintf(stderr, "%s : command not found\n", e->arguments[0]);
+      exit(EXIT_FAILURE);
+    }
+    retour = wait(NULL);
+    /*if(retour == -1)
+      afficher_prompt(1);
+    else 
+    afficher_prompt(0);*/
+  }
 }
 
-/* Parcours infixe de l'abre */
+
+void analyse_cmd(Expression * e){
+  //ecrire_history(e->arguments);
+  switch(e->type){
+  case SIMPLE:
+    executer_cmd(e);
+    break;
+  case PIPE:
+    tube(e->gauche, e->droite);
+    break;
+  default:
+    printf("Seules les commandes SIMPLES sont executées.\n");
+    break;
+  }
+}
+
+
+void afficher_prompt(int retour){
+  char * user = NULL;
+  user = getenv("USER");
+  if(user == NULL)
+    user = "unknown_user";
+  long size;
+  char *buf;
+  char *ptr;
+  size = pathconf(".", _PC_PATH_MAX);
+  if ((buf = (char *)malloc((size_t)size)) != NULL)
+    {
+      ptr = getcwd(buf, (size_t)size);
+    }
+  if(retour == 1)
+    retour = 1;
+  else retour = 0;
+  
+  printf("%s [%s] %d -$ ", user, ptr, retour);
+}
+
+
 void arbre(Expression * racine){ // parcours infixe
   if(racine != NULL){
     arbre(racine->gauche); //gauche
@@ -119,86 +185,6 @@ void arbre(Expression * racine){ // parcours infixe
     }
     arbre(racine->droite); // droite
   }
-}
-
-
-/*Recherche la commande dans le tableau des fonctions internes et l'execute si elle existe*/
-void executer_cmd(Expression * e){
-  bool trouver = false;
-  char * nom_commande = e->arguments[0];
-
-  for(int i=0; i < NB_FONCTION; i++){
-    if( strcmp(nom_fonction[i], nom_commande) == 0 ){
-      trouver = true;
-      fonction f = tableau_fonction[i];
-      int retour = (f)(e->arguments); // appel de la fonction interne
-      afficher_prompt(retour);
-      break;
-    }
-  }
-
-  if(! trouver){
-    int retour;
-    if(fork() == 0){
-      //execlp(e->arguments[0], NULL, NULL);//Ne fonctionne pas, ne tient pas compte des options
-      retour = execvp(e->arguments[0], e->arguments );
-      if(retour == -1)
-	fprintf(stderr, "%s : command not found\n", e->arguments[0]);
-      exit(EXIT_FAILURE);
-    }
-    retour = wait(NULL);
-    if(retour == -1)
-      afficher_prompt(1);
-    else afficher_prompt(0);
-  }
-
-}
-
-/* Analyse de l'expression */
-void analyse_cmd(Expression * e){
-
-  ecrire_history(e->arguments);
-
-  switch(e->type){
-  case SIMPLE:
-    executer_cmd(e); // Dans le cas d'une commande simple, on l'execute.
-    break;
-  case PIPE:
-    tube(e->gauche, e->droite);
-    break;
-  default:
-    printf("Seules les commandes SIMPLES sont executées.\n");
-    break;
-  }
-}
-
-/*Contruction du prompt */
-void afficher_prompt(int retour){
-
-  /* Recuperation de whoami */
-  char * user = NULL;
-  user = getenv("USER");//extrait le contenu de la variable d'environnemtn USER
-  if(user == NULL)
-    user = "unknown_user";
-
-  
-  /* Code de pwd() */
-  long size;
-  char *buf;
-  char *ptr;
-  
-  size = pathconf(".", _PC_PATH_MAX);
-  if ((buf = (char *)malloc((size_t)size)) != NULL)
-    {
-      ptr = getcwd(buf, (size_t)size);
-    }
-  /* Fin Code de pwd() */  
-
-  if(retour == 1)
-    retour = 1;
-  else retour = 0;
-  
-  printf("%s [%s] %d -$ ", user, ptr, retour);
 }
 
 
--- trunk/src/commande.c
+++ trunk/src/commande.c
@@ -11,26 +11,18 @@ int  pwd(char ** arguments){
   long size;
   char *buf;
   char *ptr;
-  
   size = pathconf(".", _PC_PATH_MAX);
   if ((buf = (char *)malloc((size_t)size)) != NULL)
     {
       ptr = getcwd(buf, (size_t)size);
     }
-  
-  printf("%s\n", ptr);
-
-  return 0;
-
+ printf("%s\n", ptr);
+ return 0;
 }
 
 int  cd(char ** arguments){
-  
-  int retour = chdir(arguments[1]);//c'est ce que j'ai trouver comme appel systeme,
-                      //ça marche
-  //printf("Commande cd\n");
-
-  if(retour == -1){
+ int retour = chdir(arguments[1]);
+ if(retour == -1){
     fprintf(stderr, "%s : Aucun dossier de ce type\n", arguments[1]);
     return 1;
   }
@@ -38,26 +30,21 @@ int  cd(char ** arguments){
 }
 
 int history(char ** arguments){
-
   FILE * fichier;
   fichier = fopen("history.tmp", "r");
   if(fichier == NULL){
     fprintf(stderr, "Aucun historique sauvegardé\n");
     return 1;
   }
-
   int c;
-  
-  while ((c = getc(fichier)) != EOF)
+   while ((c = getc(fichier)) != EOF)
     putc(c, stdout);
   fclose(fichier);
   fflush(stdout);
-  
   return 0;
 }
 
 int builtins(char ** arguments){
-  
   printf("Commande builtins : ");
   int i;
   for(i = 0; i < NB_FONCTION; i++)
