ac5a43f240f66369373216fe0a4aeb4f0f5781a4
momssesystem <momssesystem@9c1addf4-ab37-11dd-b08a-b13dd1f1ca2d>
2008-12-10 16:25:16 +0000

    ok

--- trunk/src/analyse_expression.c
+++ trunk/src/analyse_expression.c
@@ -18,9 +18,9 @@ char * home = NULL;
 
 /*----------- Tableau des fonctions internes(commande.h) ---------- */
 char * nom_fonction[NB_FONCTION] = { "pwd", "cd", "history", "builtins", 
-				     "kill", "times", "exit", "pushd", "popd", "dirs", "echo" };
+				     "kill", "times", "exit", "pushd", "popd", "dirs", "echo", "alias" };
 fonction  tableau_fonction[NB_FONCTION] = { pwd, cd , history, builtins, 
-					    kill_, times_, toexit, pushd, popd, dirs, echo};
+					    kill_, times_, toexit, pushd, popd, dirs, echo, alias_};
 /*-----------------------------------------------------------------*/
 
 int executer_cmd(Expression * e);
